60c0cbf1cfb4834ba5ce2817e61ea62314191706
TS-vitalik <tsiazhenkov@gmail.com>
2024-03-10 16:21:16 +0300

    add AnimationManager and README.md

---
+++ AnimationManager.hpp
@@ -0,0 +1,58 @@
+#include "Animation.hpp"
+
+/*
+AnimationManager - главный класс для отрисовки анимации объектов в игре. В нем
+хранится map названий анимаций и объектов типа Animation.
+
+Конструктор default
+
+
+Поля:
+    current_animation_ - название текущей анимации, которая будет отрисована
+(например, "animation_run") animations_list_ - map: название анимации - объект
+Animation, отвечающий за эту анимацию
+
+
+Методы:
+    Create(const std::string& animation_name, const std::vector<sf::Texture>&
+textures, double play_speed, int32_t scale) - добавляет в map анимацию Animation
+и устанавливает ее текущей
+
+
+    !!! UpdateFrame(double time) - обновляет анимацию
+    !!! Draw(sf::RenderWindow& window, int32_t x = 0, int32_t y = 0) - рисует
+текущую анимацию Animation в координатах x, y на окне window
+
+    SetAnimation(const std::string animation_name) - устанавливает текущей
+анимацию animation_name
+
+    SetFlip(bool flip) - устанавливает, нужно ли отображать анимацию зеркально
+
+    SetAnimationPause() / SetAnimationPlay() - ставит отрисовку на паузу /
+возобнавляет отрисовку (опционально)
+
+
+*/
+
+class AnimationManager {
+ public:
+  void Create(std::string animation_name,
+              const std::vector<sf::Texture>& textures, double play_speed,
+              int32_t scale);
+
+  void Draw(sf::RenderWindow& window, int32_t x = 0, int32_t y = 0);
+
+  void SetAnimation(std::string animation_name);
+
+  void SetFlip(bool flip);
+
+  void UpdateFrame(double time);
+
+  void SetAnimationPause();
+
+  void SetAnimationPlay();
+
+ private:
+  std::string current_animation_;
+  std::unordered_map<std::string, Animation> animations_list_;
+};
--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 all: main
 	./sfml-app
 
-main: main.o
-	g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+main: main.o Animation.o AnimationManager.o
+	g++ main.o Animation.o AnimationManager.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 
 main.o: main.cpp
 	g++ -c main.cpp -I /include
+
+AnimationManager.o: AnimationManager.cpp AnimationManager.hpp
+	g++ -c AnimationManager.cpp -I /include
+
+Animation.o: Animation.cpp Animation.hpp
+	g++ -c Animation.cpp -I /include
 
 clear:
 	rm sfml-app
