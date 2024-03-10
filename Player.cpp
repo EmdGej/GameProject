0f272c08f73a17326bd3c00ca41490c6ddb3a85a
TS-vitalik <tsiazhenkov@gmail.com>
2024-03-10 21:26:11 +0300

    add run and stay abilities

---
+++ Player.cpp
@@ -0,0 +1,44 @@
+#include "Player.hpp"
+
+
+Player::Player(double x_coord, double y_coord, int32_t health, int32_t damage, double acceleration) {
+  x_coord_ = x_coord;
+  y_coord_ = y_coord;
+  health_ = health;
+  damage_ = damage;
+
+  acceleration_ = acceleration;
+  is_on_ground_ = true;
+
+  STATE = stay;
+
+  FillMapWithKeys();
+}
+
+void Player::UpdatePlayer(AnimationManager& manager, double time) {
+  UpdateKeys();
+  SetState(manager);
+
+  if (direction_) {
+      manager.SetFlip(true);
+  } else {
+      manager.SetFlip(false);
+  }
+
+  y_speed_ += (!is_on_ground_) * acceleration_ * time;
+
+  x_coord_ += x_speed_ * time;
+  y_coord_ += y_speed_ * time;
+
+  manager.UpdateFrame(time);
+
+  ResetKeys();
+}
+
+void Player::SetKeys(std::string key, bool flag) {
+  keys_[key] = flag;
+}
+
+double Player::GetXCoord() { return x_coord_; }
+
+double Player::GetYCoord() { return y_coord_; }
---
+++ Player.hpp
@@ -0,0 +1,164 @@
+/*
+Player - класс игрока в игре, унаследованный от базовой сущности AbstractEntity
+
+Конструктор:
+  x_coord - координата x, 
+  y_coord - координата y, 
+  health - здоровье игрока, 
+  damage - урон игрока,
+  acceleration - ускорение игрока
+
+
+Поля: (часть унаследована от AbstractEntity)
+  enum {stay, run, jump, slide, climb} STATE - возможные состояния игрока
+  std::unordered_map<std::string, bool> keys_ - состояния используемых в игре кнопок
+  double acceleration_ - ускорение игрока
+  bool direction_ = false; // 0 - left, 1 - right - направление движения игрока
+  const double kSpeedX = 0.2 - стандартная скорость игрока по x
+  const double kSpeedY = 0.5 - стандартная скорость игрока по y
+
+  bool prev_direction = false; // 0 - left, 1 - right - отслеживание одновременных нажатий
+
+
+Методы:
+  UpdatePlayer - обновление анимации и положения игрока
+
+  SetState - устанавливает текущее состояние игрока
+
+  UpdateKeys - обновляет нажатые кнопки
+
+  ResetKeys - сбрасывает состояния нажатых кнопок
+
+  FillMapWithKeys - заполняет map используемыми кнопками
+*/
+
+
+#include "AbstractEntity.hpp"
+
+class Player: public AbstractEntity {
+ public:
+  Player(double x_coord, double y_coord, int32_t health, int32_t damage, double acceleration = 0);
+
+  void UpdatePlayer(AnimationManager& manager, double time);
+
+  void SetKeys(std::string key, bool flag);
+
+  double GetXCoord();
+
+  double GetYCoord();
+  
+ private:
+  enum {stay, run, jump, slide, climb} STATE;
+  std::unordered_map<std::string, bool> keys_;
+  
+  double acceleration_ = 0;
+  
+  bool direction_ = false; // 0 - left, 1 - right
+  bool prev_direction = false; // 0 - left, 1 - right
+  
+  const double kSpeedX = 0.2;
+  const double kSpeedY = 0.5;
+
+  void SetState(AnimationManager& manager) {
+    if (STATE == stay) {
+      manager.SetAnimation("stay");
+    }
+
+    if (STATE == run) {
+      manager.SetAnimation("run");
+    }
+
+    if (STATE == jump) {
+      manager.SetAnimation("jump");
+    }
+
+    if (STATE == slide) {
+      manager.SetAnimation("slide");
+    }
+
+    if (STATE == climb) {
+      manager.SetAnimation("climb");
+    }
+  }
+
+  void UpdateKeys() {
+    //============================== STAY ============================== //
+
+    if (!(keys_["ArrowLeft"]) && !(keys_["ArrowRight"]) && is_on_ground_) {
+      STATE = stay;
+      x_speed_ = 0;
+    }
+
+    //============================== RUN ============================== //
+    if(keys_["ArrowLeft"] && keys_["ArrowRight"]) {
+        if(!prev_direction) {
+            direction_ = !direction_;
+            x_speed_ = -x_speed_;
+        }
+
+        prev_direction = true;
+    } else if (keys_["ArrowLeft"]) {
+        if (STATE == stay) {
+            STATE = run;
+        }
+        x_speed_ = -kSpeedX;
+
+        if (STATE == jump) {
+            x_speed_ = -kSpeedX;
+        }
+        
+        prev_direction = false;
+        direction_ = 0;
+    } else if (keys_["ArrowRight"]) {
+        if (STATE == stay) {
+            STATE = run;
+        }
+        x_speed_ = kSpeedX;
+        
+        if (STATE == jump) {
+            x_speed_ = kSpeedX;
+            direction_ = 1;
+        }
+        
+        prev_direction = false;
+        direction_ = 1;       
+    }
+
+    //============================== JUMP ============================== //
+    if (keys_["Z"]) {
+      if (STATE == stay || STATE == run || STATE == slide || STATE == climb) {
+        STATE = jump;
+        y_speed_ = kSpeedY;
+        
+        is_on_ground_ = false;
+      }
+    }
+
+    //============================== SHOOT ============================== //
+    if (keys_["X"]) {
+      is_shooting_ = true;
+    }
+
+    //============================== SLIDE ============================== //
+    if (keys_["ArrowDown"]) {
+      if (STATE == run) {
+        STATE = slide;
+      }
+    }
+  }
+
+  void ResetKeys() {
+    for (auto& key_state: keys_) {
+      key_state.second = false;
+    }
+  }
+
+  // X - стрельба, Z - прыжок, C - опциональное, Shift - ускоренный бег
+  void FillMapWithKeys() {
+    std::vector<std::string> keys = {"LeftArrow", "RightArrow", "UpArrow", "DownArrow", "Space", "Z", "X", "C", "Shift"};
+
+    for (size_t i = 0; i < keys.size(); ++i) {
+      keys_[keys[i]] = false;
+    }
+  }
+};
