360d16c3b3c46ebaec5f5d0a2eb046131c704e0b
TS-vitalik <tsiazhenkov@gmail.com>
2024-03-13 23:41:14 +0300

    add bullet manager classes

---
+++ BulletManager.hpp
@@ -0,0 +1,25 @@
+#ifndef BULLET_MANAGER
+#define BULLET_MANAGER
+
+#include <list>
+
+#include "AnimationManager.hpp"
+#include "Bullet.hpp"
+#include "MapLoader.hpp"
+
+class BulletManager {
+ public:
+  void AddBullet(AnimationManager manager, double bullet_speed,
+                 bool player_direction, double player_x, double player_y,
+                 double player_width, double player_height);
+
+  void CheckLifeBullets();
+
+  void UpdateDrawBullets(sf::RenderWindow& window, const MapParams& map_params,
+                         const std::unordered_set<char>& blocks, double time);
+
+ private:
+  std::list<Bullet*> bullets_;
+};
+
+#endif
