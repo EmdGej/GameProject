62ce17eb28911b7c47e0cff8d95fb5c0fbb6ec6d
TS-vitalik <tsiazhenkov@gmail.com>
2024-03-14 17:18:28 +0300

    add map

---
+++ Map.hpp
@@ -0,0 +1,52 @@
+#ifndef MAP
+#define MAP
+
+#include <vector>
+#include <string>
+
+namespace Map {
+  const int Height = 35;
+  const int Width = 100;
+
+  std::vector<std::string> tilemap = {
+    
+    "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    "BBBBBBBBBBBBBBBBBBBBB                                                                              B",
+    "BB                  B                                                                              B",
+    "BB                  B                                                                              B",
+    "BB                  B                                                                              B",
+    "BB                  B                                                                              B",
+    "BB                  B                                                                              B",
+    "BB        FFF       B                                                                              B",
+    "BB                  B                                                                              B",
+    "BB    FF       FF   B                                                                              B",
+    "BB                  B                                                                              B",
+    "BB                                                                            S   S                B",
+    "BB                                                            FFFFFFFFFFFFFFFFFFFFF                B",
+    "BB                                                            BBBBBBBBBBBBBBBBBBBBB                B",
+    "BBFFFFFFFFFFFFFFFFFFFFFF                                   FFFBBBBBBBBBBBBBBBBBBBBB                B",
+    "B                                                             BBBBBBBBBBBBBBBBBBBBB                B",
+    "B                                                             BBBBBBBBBBBBBBBBBBBBB                B",
+    "B                                                             BBBBBBBBBBBBBBBBBBBBB                B",
+    "B                                       SSS                   BBBBBBBBBBBBBBBBB                    B",
+    "B                             FFFFF  HHHHHHHHH  FFF           BBBBBBBBBBBBBBBBB                    B",
+    "B                             BBBBBSSSSSSSSSSSSSB      FFFFFFFBBBBBBBBBBBBBBBBB                    B",
+    "B                             BBBBBBBBBBBBBBBBBB       BBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                                      BBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                                      BBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                                      BBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                                      BBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                                  S   BBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                            FFFFFFFFFFBBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                            BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                            BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                     HHHHHH BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                                            BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B                FFFFFFFFF     FFFF   HHHHHH BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                    B",
+    "B             FFFBBBBBBBBBSSSSSBBBBSSSSSSSSSSBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBSSSSSSSSSFSSSSSSSSSSB",
+    "BFFFFFFFFFFFFFBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+  
+  };
+}
+
+#endif
