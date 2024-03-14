115b254fac556d3d20c46c49a09371ddc4a0c57d
TS-vitalik <tsiazhenkov@gmail.com>
2024-03-14 13:56:27 +0300

    update CollisionManager

--- CollisionManager.cpp
+++ CollisionManager.cpp
@@ -3,8 +3,10 @@
 void CollisionManager::CheckCollisions(Player& player, std::vector<Enemy>& enemies) {
   for (auto& enemy: enemies) {
     if (sf::Rect<double>(player.GetXCoord(), player.GetYCoord(), player.GetAnimationWidth(), player.GetAnimationHeight()).intersects(sf::Rect<double>(enemy.GetXCoord(), enemy.GetYCoord(), enemy.GetAnimationWidth(), enemy.GetAnimationHeight()))) {
-      player.SetHealth(0);
-      return;
+      if (enemy.GetIsAlive()) {
+        player.SetHealth(0);
+        return;
+      }
     }
   }
 }
